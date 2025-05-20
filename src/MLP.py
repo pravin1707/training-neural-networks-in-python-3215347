9f4d86f580850050a650f3e62841d06276917f2f
pravin1707 <pravin1712kumar@gmail.com>
2025-05-20 11:11:29 -0400

    multilayer perceptron def_init

--- src/MLP.py
+++ src/MLP.py
@@ -40,7 +40,13 @@ class MultiLayerPerceptron:
         self.network = [] # The list of lists of neurons
         self.values = []  # The list of lists of output values        
         
-
+        for i in range(len(self.layers)):
+            self.values.append([])
+            self.network.append([])
+            self.values[i] = [0.0 for j in range(self.layers[i])]
+            if(i > 0):
+                for j in range(self.layers[i]):
+                    self.network[i].append(Perceptron(inputs=self.layers[i-1], bias=self.bias))
         
         self.network = np.array([np.array(x) for x in self.network],dtype=object)
         self.values = np.array([np.array(x) for x in self.values],dtype=object)
